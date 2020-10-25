ab76eefb424952e3acc197ff1485d22b2ff1877a
JuanTrigueros99 <juan.d.trigueros@gmail.com>
2020-10-25 04:33:57 +0000

    Server runs on port, main & create quiz pages load, rest is broken links

---
+++ public/js/create.js
@@ -0,0 +1,24 @@
+var socket = io();
+
+socket.on('connect', function(){
+    socket.emit('request_db_names');//Get database names to display to user
+});
+
+socket.on('game_names_data', function(data){
+    for(var i = 0; i < Object.keys(data).length; i++){
+        var div = document.getElementById('game-list');
+        var button = document.createElement('button');
+        
+        button.innerHTML = data[i].name;
+        button.setAttribute('onClick', "startGame('" + data[i].id + "')");
+        button.setAttribute('id', 'gameButton');
+        
+        div.appendChild(button);
+        div.appendChild(document.createElement('br'));
+        div.appendChild(document.createElement('br'));
+    }
+});
+
+function startGame(data){
+    window.location.href="/host/" + "?id=" + data;
+}
